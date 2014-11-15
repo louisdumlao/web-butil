25e721985bda3744387c461057994eeeed64e59e
Rhett Jason C. Buzon <rhettbuzon@gmail.com>
2014-11-15 18:25:17 +0800

    Sync /vendor and configured database.php
    -Excluded /vendor in .gitignore. No need for each to execute 'composer install'

--- SimpleLaravel/app/config/database.php
+++ SimpleLaravel/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
+			'database'  => 'butil',
 			'username'  => 'root',
-			'password'  => 'Introse',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
