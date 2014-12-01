09f533b434d7488f889c39c914bd357312d99c65
louisdumlao <louis.dumlao@yahoo.com>
2014-12-02 02:39:21 +0800

    Quick layout updates

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -81,10 +81,7 @@
     <!-- end add plant modal -->
 
     <!-- grid/list view control -->
-    <div class="btn-group pull-right">
-        <a class="btn btn-default active" id="list-view"><i class="fa fa-list"></i></a>
-        <a class="btn btn-default" id="grid-view"><i class="fa fa-image"></i></a>
-    </div>
+
 
     <!-- Main content -->
     <div class="container center">
@@ -98,194 +95,198 @@
                 <a class="btn btn-default disabled btn-curr"><i class="fa fa-trash"></i> Delete</a>
                 </span>
             </span>
+            <div class="btn-group pull-right">
+                <a class="btn btn-default active" id="list-view"><i class="fa fa-list"></i></a>
+                <a class="btn btn-default" id="grid-view"><i class="fa fa-image"></i></a>
+            </div>
         </div>
         <div class="row">
-            <div class="grid-view hidden table-bordered-out">
-                @for ($i = 1; $i <=2 ; $i++) 
-                   <a href="#" class="col-xs-2 grid-element">
+            <div class="grid-view hidden table-bordered-out col-sm-12">
+                @for ($i = 1; $i
+                <=2 ; $i++) <a href="#" class="col-xs-2 grid-element">
                     <div class="small-box bg-highlight">
                         <div class="inner">
                             <img src="img/img{{$i % 2}}.jpg">
                         </div>
-                            <div class="small-box-footer">
-                                <p>Plant {{$i}}</p>
-                                <p>Mid-Tillering</p>
-                                <p>2014-11-19</p>
-                            </div>
+                        <div class="small-box-footer">
+                            <p>Plant {{$i}}</p>
+                            <p>Mid-Tillering</p>
+                            <p>2014-11-19</p>
                         </div>
-                        </a>
-                        @endfor
                     </div>
-                    <div class="list-view col-sm-12">
-                        <table class="table table-bordered-out table-hover table-condensed table-responsive">
-                            <thead>
-                                <tr>
-                                    <th>
-                                        <input type="checkbox" id="currPlantSelectAll">
-                                    </th>
-                                    <th>Camera</th>
-                                    <th>Plant Name</th>
-                                    <th></th>
-                                    <th>Date Last Phenotyped</th>
-                                    <th>Biomass (cm3)</th>
-                                    <th>Greenness</th>
-                                    <th>Height</th>
-                                    <th>Tiller Count</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <!-- plant values here; use data-url for navigating to plant phenotypic history menu; fa-pencil for edit modal -->
-                                <tr class="clickable-row" data-url="plant">
-                                    <td>
-                                        <input type="checkbox" class="checkbox-curr-plant">
-                                    </td>
-                                    <td>1L</td>
-                                    <td>IR64-IRS007-006</td>
-                                    <td><i class="fa fa-pencil fa-hidden" data-toggle="modal" data-target=".addplant"></i>
-                                    </td>
-                                    <td>2014-11-11</td>
-                                    <td>43</td>
-                                    <td>Between 3 and 4</td>
-                                    <td>40.63</td>
-                                    <td>1</td>
-                                </tr>
-                                <tr class="clickable-row" data-url="plant">
-                                    <td>
-                                        <input type="checkbox" class="checkbox-curr-plant">
-                                    </td>
-                                    <td>1R</td>
-                                    <td>Sample Plant 5</td>
-                                    <td><i class="fa fa-pencil fa-hidden"></i>
-                                    </td>
-                                    <td>2014-10-18</td>
-                                    <td>45</td>
-                                    <td>3</td>
-                                    <td>51.12</td>
-                                    <td>17.40</td>
-                                </tr>
-                                <tr data-toggle="modal" data-target=".addplant">
-                                    <td class="btn-addplant" colspan="9">+ Add Plant</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                    </a>
+                    @endfor
             </div>
-        </div>
-        <div class="container center">
-            <div class="row">
-                <span class="table-header">
-            <span class="col-sm-3">
-            Archived Plants
-            </span>
-                <span class="btn-group col-sm-6">
-            <a class="btn btn-default disabled btn-arch"><i class="fa fa-arrow-circle-up"></i> Move to Current</a>
-            <a class="btn btn-default disabled btn-arch"><i class="fa fa-trash"></i> Delete</a>
-            </span>
-                </span>
-            </div>
-            <div class="row">
-                <div class="grid-view hidden table-bordered-out">
-                @for ($i = 1; $i <=7 ; $i++) 
-                   <a href="#" class="col-xs-2 grid-element">
-                    <div class="small-box bg-highlight">
-                        <div class="inner">
-                            <img src="img/img{{$i % 2}}.jpg">
-                        </div>
-                            <div class="small-box-footer">
-                                <p>Plant {{$i}}</p>
-                                <p>Mid-Tillering</p>
-                                <p>2013-11-24</p>
-                            </div>
-                        </div>
-                        </a>
-                        @endfor
-                    </div>
-                <div class="list-view col-sm-12">
-                    <table class="table table-bordered-out table-hover table-condensed table-responsive">
-                        <thead>
-                            <tr>
-                                <th>
-                                    <input type="checkbox" id="archPlantSelectAll">
-                                </th>
-                                <th>Camera</th>
-                                <th>Plant Name</th>
-                                <th></th>
-                                <th>Date Last Phenotyped</th>
-                                <th>Biomass (cm3)</th>
-                                <th>Greenness</th>
-                                <th>Height</th>
-                                <th>Tiller Count</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="clickable-row" data-url="plant">
-                                <td>
-                                    <input type="checkbox" class="archPlantRowSelect">
-                                </td>
-                                <td>1L</td>
-                                <td>IR64-C-IRS001-001</td>
-                                <td><i class="fa fa-pencil fa-hidden"></i>
-                                </td>
-                                <td>2014-08-02</td>
-                                <td>24</td>
-                                <td>Between 3 and 4</td>
-                                <td>46.50</td>
-                                <td>15.50</td>
-                            </tr>
-                            <tr class="clickable-row" data-url="plant">
-                                <td>
-                                    <input type="checkbox" class="archPlantRowSelect">
-                                </td>
-                                <td>1R</td>
-                                <td>Sample Plant 2</td>
-                                <td><i class="fa fa-pencil fa-hidden"></i>
-                                </td>
-
-                                <td>2014-06-18</td>
-                                <td>45</td>
-                                <td>3</td>
-                                <td>21.40</td>
-                                <td>58.16</td>
-                            </tr>
-                            <tr class="clickable-row" data-url="plant">
-                                <td>
-                                    <input type="checkbox" class="archPlantRowSelect">
-                                </td>
-                                <td>2L</td>
-                                <td>Sample Plant 3</td>
-                                <td><i class="fa fa-pencil fa-hidden"></i>
-                                </td>
-
-                                <td>2013-01-12</td>
-                                <td>38</td>
-                                <td>Between 2 and 3</td>
-                                <td>61.24</td>
-                                <td>16.32</td>
-                            </tr>
-                            <tr class="clickable-row" data-url="plant">
-                                <td>
-                                    <input type="checkbox" class="archPlantRowSelect">
-                                </td>
-                                <td>3R</td>
-                                <td>Sample Plant 4</td>
-                                <td><i class="fa fa-pencil fa-hidden"></i>
-                                </td>
-                                <td>2011-06-16</td>
-                                <td>41.2</td>
-                                <td>2</td>
-                                <td>28.14</td>
-                                <td>13.22</td> 
-                            </tr>
-                            <tr href="#">
-                                    <td class="btn-viewmore" colspan="9">View More...</td>
-                                </tr>
-                        </tbody>
-                    </table>
-                </div>
+            <div class="list-view col-sm-12">
+                <table class="table table-bordered-out table-hover table-condensed table-responsive">
+                    <thead>
+                        <tr>
+                            <th>
+                                <input type="checkbox" id="currPlantSelectAll">
+                            </th>
+                            <th>Camera</th>
+                            <th>Plant Name</th>
+                            <th></th>
+                            <th>Date Last Phenotyped</th>
+                            <th>Biomass (cm3)</th>
+                            <th>Greenness</th>
+                            <th>Height</th>
+                            <th>Tiller Count</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- plant values here; use data-url for navigating to plant phenotypic history menu; fa-pencil for edit modal -->
+                        <tr class="clickable-row" data-url="plant">
+                            <td>
+                                <input type="checkbox" class="checkbox-curr-plant">
+                            </td>
+                            <td>1L</td>
+                            <td>IR64-IRS007-006</td>
+                            <td><i class="fa fa-pencil fa-hidden" data-toggle="modal" data-target=".addplant"></i>
+                            </td>
+                            <td>2014-11-11</td>
+                            <td>43</td>
+                            <td>Between 3 and 4</td>
+                            <td>40.63</td>
+                            <td>1</td>
+                        </tr>
+                        <tr class="clickable-row" data-url="plant">
+                            <td>
+                                <input type="checkbox" class="checkbox-curr-plant">
+                            </td>
+                            <td>1R</td>
+                            <td>Sample Plant 5</td>
+                            <td><i class="fa fa-pencil fa-hidden"></i>
+                            </td>
+                            <td>2014-10-18</td>
+                            <td>45</td>
+                            <td>3</td>
+                            <td>51.12</td>
+                            <td>17.40</td>
+                        </tr>
+                        <tr data-toggle="modal" data-target=".addplant">
+                            <td class="btn-addplant" colspan="9">+ Add Plant</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
-    <!-- ./wrapper -->
+    <div class="container center">
+        <div class="row">
+            <span class="table-header">
+            <span class="col-sm-3">
+            Archived Plants
+            </span>
+            <span class="btn-group col-sm-6">
+            <a class="btn btn-default disabled btn-arch"><i class="fa fa-arrow-circle-up"></i> Move to Current</a>
+            <a class="btn btn-default disabled btn-arch"><i class="fa fa-trash"></i> Delete</a>
+            </span>
+            </span>
+        </div>
+        <div class="row">
+            <div class="grid-view hidden table-bordered-out">
+                @for ($i = 1; $i
+                <=7 ; $i++) <a href="#" class="col-xs-2 grid-element">
+                    <div class="small-box bg-highlight">
+                        <div class="inner">
+                            <img src="img/img{{$i % 2}}.jpg">
+                        </div>
+                        <div class="small-box-footer">
+                            <p>Plant {{$i}}</p>
+                            <p>Mid-Tillering</p>
+                            <p>2013-11-24</p>
+                        </div>
+                    </div>
+                    </a>
+                    @endfor
+            </div>
+            <div class="list-view col-sm-12">
+                <table class="table table-bordered-out table-hover table-condensed table-responsive">
+                    <thead>
+                        <tr>
+                            <th>
+                                <input type="checkbox" id="archPlantSelectAll">
+                            </th>
+                            <th>Camera</th>
+                            <th>Plant Name</th>
+                            <th></th>
+                            <th>Date Last Phenotyped</th>
+                            <th>Biomass (cm3)</th>
+                            <th>Greenness</th>
+                            <th>Height</th>
+                            <th>Tiller Count</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="clickable-row" data-url="plant">
+                            <td>
+                                <input type="checkbox" class="archPlantRowSelect">
+                            </td>
+                            <td>1L</td>
+                            <td>IR64-C-IRS001-001</td>
+                            <td><i class="fa fa-pencil fa-hidden"></i>
+                            </td>
+                            <td>2014-08-02</td>
+                            <td>24</td>
+                            <td>Between 3 and 4</td>
+                            <td>46.50</td>
+                            <td>15.50</td>
+                        </tr>
+                        <tr class="clickable-row" data-url="plant">
+                            <td>
+                                <input type="checkbox" class="archPlantRowSelect">
+                            </td>
+                            <td>1R</td>
+                            <td>Sample Plant 2</td>
+                            <td><i class="fa fa-pencil fa-hidden"></i>
+                            </td>
+
+                            <td>2014-06-18</td>
+                            <td>45</td>
+                            <td>3</td>
+                            <td>21.40</td>
+                            <td>58.16</td>
+                        </tr>
+                        <tr class="clickable-row" data-url="plant">
+                            <td>
+                                <input type="checkbox" class="archPlantRowSelect">
+                            </td>
+                            <td>2L</td>
+                            <td>Sample Plant 3</td>
+                            <td><i class="fa fa-pencil fa-hidden"></i>
+                            </td>
+
+                            <td>2013-01-12</td>
+                            <td>38</td>
+                            <td>Between 2 and 3</td>
+                            <td>61.24</td>
+                            <td>16.32</td>
+                        </tr>
+                        <tr class="clickable-row" data-url="plant">
+                            <td>
+                                <input type="checkbox" class="archPlantRowSelect">
+                            </td>
+                            <td>3R</td>
+                            <td>Sample Plant 4</td>
+                            <td><i class="fa fa-pencil fa-hidden"></i>
+                            </td>
+                            <td>2011-06-16</td>
+                            <td>41.2</td>
+                            <td>2</td>
+                            <td>28.14</td>
+                            <td>13.22</td>
+                        </tr>
+                        <tr href="#">
+                            <td class="btn-viewmore" colspan="9">View More...</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ./wrapper -->
 
 
-    @stop
+@stop
